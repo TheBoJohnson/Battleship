4b19709a2c65e44aa94114a7147cb452e3c30461
Bo Johnson <yobo.johnson@gmail.com>
2018-03-06 20:08:54 -0700

    Finished the model for the Battleship game

--- model.cpp
+++ model.cpp
@@ -12,107 +12,29 @@
 
 using namespace std;
 
-// base class for the ocean model
-class OceanModel{
-	public:
-		// Default Contructor for the Ocean Model
-		OceanModel();
-
-		void changeTile(int x, int y, int type);
-
-	protected:
-		// Array representation of the Ocean
-		int oceanArray[10][10];
-};
-
-OceanModel::OceanModel(){
-	for(int i = 0; i < 10; i++){
-		for(int j = 0; j < 10; j++){
-			oceanArray[i][j] = EMPTY;
-		}
-	}
-}
-
-void OceanModel::changeTile(int x, int y, int type){
-	oceanArray[x][y] = type; 
-}
-
-
-
-// class for the User ocean
-class userOceanModel : public OceanModel{
-	public:
-		userOceanModel();
-		bool checkIfOccupied(int startX, int startY, int orientation, int size);
-
-	private:
-		void importShip(Ship toAdd);
-
-};
-
-void userOceanModel::importShip(Ship toAdd){
-	if(toAdd.getOrientation == VERT){
-		if(toAdd.getSize() + toAdd.getStartY > 10){
-			cout << "Ship is not in bounds" << endl;
-		}
-		else if(!checkIfOccupied(toAdd.getStartX(), toAdd.getStartY(), toAdd.getOrientation(), toAdd.getSize())){
-			cout << "You are trying to place the ship in tiles that are already occupied" << endl;
-		}
-		else{
-			for(int i = startY; i < startY + size; i++){
-				oceanArray[startX][startY] = SHIP;
-			}
-		}
-	}
-
-}
-
-
-bool userOceanModel::checkIfOccupied(int startX, int startY, int orientation, int size){
-	if(orientation == VERT){
-		for(int i = startY; i < startY + size; i++){
-			if(oceanArray[startX][i] == SHIP){
-				return false;
-			}
-		}
-		return true;
-	}
-	else{
-		for(int i = startX; i < startX + size; i++){
-			if(oceanArray[i][startY] == SHIP){
-				return false;
-			}
-		}
-		return true;
-	}
-}
-
-// class for the enemy ocean
-
-
-
-
 
 //class for the ships
-
 class Ship{
 	public:
 		Ship(int s, bool o, int sX, int sY);
 
 		int getSize();
 		bool getOrientation();
-		int getStartX; 
-		int getStartY;
+		int getStartX(); 
+		int getStartY();
+		int getLife();
+		void takeLife();
 
 	private:
 		int size;
 		bool orientation;
 		int startX; 
 		int startY;
+		int life; 
 };
 
-void Ship::Ship(int s, int o, int sX, int sY){
-	size = s;
+Ship::Ship(int s, bool o, int sX, int sY){
+	size = life = s;
 	orientation = o;
 	startX = sX; 
 	startY = sY;
@@ -134,10 +56,120 @@ int Ship::getStartY(){
 	return startY;
 }
 
-int main(){
+int Ship::getLife(){
+	return life; 
+}
+
+void Ship::takeLife(){
+	life--;
+}
+
+
+// base class for the ocean model
+class OceanModel{
+	public:
+		// Default Contructor for the Ocean Model
+		OceanModel();
+
+		void changeTile(int x, int y, int type);
+
+	protected:
+		// Array representation of the Ocean
+		int oceanArray[10][10];
+};
+OceanModel::OceanModel(){
+	for(int i = 0; i < 10; i++){
+		for(int j = 0; j < 10; j++){
+			oceanArray[i][j] = EMPTY;
+		}
+	}
+}
+
+void OceanModel::changeTile(int x, int y, int type){
+	oceanArray[x][y] = type; 
+}
 
 
 
+// class for the User ocean
+class userOceanModel : public OceanModel{
+	public:
+		userOceanModel();
+		bool checkIfOccupied(int startX, int startY, bool orientation, int size);
+		void importShip(Ship& toAdd);
+};
 
-	return 0;
+userOceanModel::userOceanModel(){
+	OceanModel();
+}
+
+void userOceanModel::importShip(Ship& toAdd){
+	if(toAdd.getOrientation() == VERT){
+		if(toAdd.getSize() + toAdd.getStartY() > 9){
+			cout << "Ship is not in bounds" << endl;
+		}
+		else if(!checkIfOccupied(toAdd.getStartX(), toAdd.getStartY(), toAdd.getOrientation(), toAdd.getSize())){
+			cout << "You are trying to place the ship in tiles that are already occupied" << endl;
+		}
+		else{
+			for(int i = toAdd.getStartY(); i < toAdd.getStartY() + toAdd.getSize(); i++){
+				oceanArray[i][toAdd.getStartX()] = SHIP;
+			}
+		}
+	}
+	else{
+		if(toAdd.getSize() + toAdd.getStartX() > 9){
+			cout << "The Ship is not in bounds" << endl;
+		}
+		else if(!checkIfOccupied(toAdd.getStartX(), toAdd.getStartY(), toAdd.getOrientation(), toAdd.getSize())){
+			cout << "You are trying to place the ship in tiles that are alrady occupied" << endl;
+		}
+		else{
+			for(int i = toAdd.getStartX(); i < toAdd.getStartX() + toAdd.getSize(); i++){
+				oceanArray[toAdd.getStartY()][i] = SHIP;
+			}
+		}
+	}
+}
+
+
+bool userOceanModel::checkIfOccupied(int startX, int startY, bool orientation, int size){
+	if(orientation == VERT){
+		for(int i = startY; i < startY + size; i++){
+			if(oceanArray[startX][i] == SHIP){
+				return false;
+			}
+		}
+		return true;
+	}
+	else{
+		for(int i = startX; i < startX + size; i++){
+			if(oceanArray[i][startY] == SHIP){
+				return false;
+			}
+		}
+		return true;
+	}
+}
+
+// class for the enemy ocean
+class enemyOceanModel : public OceanModel{
+	public:
+		enemyOceanModel();
+		void markHit(int xPos, int yPos);
+		void markMiss(int xPos, int yPos);
+};
+
+// constructor
+enemyOceanModel::enemyOceanModel(){
+	OceanModel();
+}
+
+
+void enemyOceanModel::markHit(int xPos, int yPos){
+	changeTile(xPos, yPos, HIT);
+}
+
+void enemyOceanModel::markMiss(int xPos, int yPos){
+	changeTile(xPos, yPos, MISS); 
 }
